42a51ba2041fcc3071621cfe2745d4b17a155609
Junya Ishihara <junya@champierre.com>
2016-05-29 22:13:48 +0900

    Enable to set steps/degrees.

--- scratch2mip.js
+++ scratch2mip.js
@@ -5,52 +5,52 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.right = function(str) {
-        $.get('http://localhost:8080/right', null, function() {
-            console.log('turn right');
+    ext.right = function(degrees) {
+        $.get('http://localhost:8080/right?degrees=' + degrees, null, function() {
+            console.log('turn right ' + degrees + ' degrees');
         });
     };
     
-    ext.left = function(str) {
-        $.get('http://localhost:8080/left', null, function() {
-            console.log('turn left');
+    ext.left = function(degrees) {
+        $.get('http://localhost:8080/left?degrees=' + degrees, null, function() {
+            console.log('turn left ' + degrees + ' degrees');
         });
     }
     
-    ext.forward = function(str) {
-        $.get('http://localhost:8080/forward', null, function() {
-            console.log('forward');
+    ext.forward = function(steps) {
+        $.get('http://localhost:8080/forward?steps=' + steps, null, function() {
+            console.log('move forward ' + steps + ' steps');
         });
     };
     
-    ext.backward = function(str) {
-        $.get('http://localhost:8080/backward', null, function() {
-            console.log('backward');
+    ext.backward = function(steps) {
+        $.get('http://localhost:8080/backward?steps=' + steps, null, function() {
+            console.log('move backward ' + steps + ' steps');
         });
     }
 
     var lang = ((navigator.language || navigator.userLanguage) == 'ja') ? 'ja' : 'en';
     var locale = {
         ja: {
-            turn_right: '右に回す',
-            turn_left: '左に回す',
-            move_forward: '前進させる',
-            move_backward: '後退させる'
+            turn_right: '右に %n 度回す',
+            turn_left: '左に %n 度回す',
+            move_forward: '%n 歩前進させる',
+            move_backward: '%n 歩後退させる'
         },
         en: {
-            turn_right: 'Turn Right',
-            turn_left: 'Turn Left',
-            move_forward: 'Move Forward',
-            move_backward: 'Move Backward'
+            turn_right: 'turn right %n degrees',
+            turn_left: 'turn left %n degrees',
+            move_forward: 'move forward %n steps',
+            move_backward: 'move backward %n steps'
         },
     }
 
     var descriptor = {
         blocks: [
-            [' ', 'MiP: ' + locale[lang].turn_right, 'right', 'sample text'],
-            [' ', 'MiP: ' + locale[lang].turn_left, 'left', 'sample text'],
-            [' ', 'MiP: ' + locale[lang].move_forward, 'forward', 'sample text'],
-            [' ', 'MiP: ' + locale[lang].move_backward, 'backward', 'sample text']
+            [' ', 'MiP: ' + locale[lang].turn_right, 'right', 90],
+            [' ', 'MiP: ' + locale[lang].turn_left, 'left', 90],
+            [' ', 'MiP: ' + locale[lang].move_forward, 'forward'],
+            [' ', 'MiP: ' + locale[lang].move_backward, 'backward']
         ]
     };
 
