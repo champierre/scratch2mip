f74c79ca94ca7459a8d1a8cb40b9e57e679b5dd6
Michael Klepikov <klm@google.com>
2016-10-01 00:19:09 -0400

    Set radar/gesture mode; get last radar status, last gesture.

--- scratch2mip_helper/scratch2mip_helper.js
+++ scratch2mip_helper/scratch2mip_helper.js
@@ -12,6 +12,9 @@ var selectedRobot;
 var readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout);
 
+var lastRadar;
+var lastGesture;
+
 function handleRequest(request, response) {
   try {
     //log the request on console
@@ -34,6 +37,40 @@ function getQueryObj(req) {
   // var obj = JSON.parse( queryObj.jsonData );
 }
 
+function radarFromCode(status) {
+    switch (status) {
+    case 0x01:
+        return 'clear';
+    case 0x02:
+        return 'far';  // 10-30 cm
+    case 0x03:
+        return 'near';  // 0-10 cm
+    default:
+        return undefined;
+    }
+}
+
+function gestureFromCode(status) {
+    switch (status) {
+    case 0x0a:
+        return 'left';
+    case 0x0b:
+        return 'right';
+    case 0x0c:
+        return 'center sweep left';
+    case 0x0d:
+        return 'center sweep right';
+    case 0x0e:
+        return 'center hold';
+    case 0x0f:
+        return 'forward';
+    case 0x10:
+        return 'back';
+    default:
+        return undefined;
+    }
+}
+
 mipFinder.scan(function(err, robots) {
   if (err != null) {
     console.log(err);
@@ -52,6 +89,33 @@ mipFinder.scan(function(err, robots) {
           console.log(err);
           return;
         }
+
+		var ignoreList = {"GET_STATUS":true, "GET_WEIGHT_LEVEL":true};
+		var ignore = true;
+
+		//setup receive data notification
+		selectedRobot.enableBTReceiveDataNotification(true, function(err, data) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+
+			//convert the response by MiPCommand
+			selectedRobot.convertMiPResponse(data, function(command, arr) {
+                if (command === 'SET_RADAR_MODE') {
+                    lastRadar = radarFromCode(arr[0]);
+                    console.info('Radar: %s', lastRadar);
+                    return;
+                } else if (command === 'SET_GESTURE_MODE') {
+                    lastGesture = gestureFromCode(arr[0]);
+                    console.info('Gesture: %s', lastGesture);
+                    return;
+                }
+				if (!ignore || ignoreList[command] === undefined || !ignoreList[command]) {
+					console.log("> "+command+": "+arr);
+				}
+			});
+		});
 
         //Create a server
         var server = http.createServer(handleRequest);
@@ -104,4 +168,37 @@ dispatcher.onGet('/left', function(req, res) {
   selectedRobot.driveDistanceByCm(0, degrees * -1, function(err) {
     console.log('Drive to Left ' + degrees + ' degrees');
   });
+});
+
+dispatcher.onGet('/set-radar-mode', function(req, res) {
+  var mode = getQueryObj(req).mode;
+  var modeCode;
+  if (mode === 'radar') {
+      modeCode = 4;
+  } else if (mode === 'gesture') {
+      modeCode = 2;
+  } else {
+      console.error('Invalid radar mode "%s", must be one of {radar, gesture}', mode);
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.end('Invalid radar mode: ' + mode + '\r\nset-radar-mode');
+      return;
+  }
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('set-radar-mode');
+  selectedRobot.sendMiPCommand("SET_RADAR_MODE", modeCode, function(err) {
+      lastRadar = undefined;
+      lastGesture = undefined;
+      console.info('Set radar mode = %s', mode);
+  });
+});
+
+dispatcher.onGet('/get-radar', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(lastRadar + '\r\nget-radar');
+});
+
+dispatcher.onGet('/get-gesture', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(lastGesture + '\r\nget-gesture');
+  lastGesture = undefined;
 });
